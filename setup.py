87f37551f1e0c3b0179a867a99dce53320b7a87f
Jason Livesay <runvnc@gmail.com>
2025-02-21 08:45:11 -0600

    init

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(
-    name="mr_groq",
+    name="mr_grok",
     version="1.0.0",
     packages=find_packages(where="src"),
     package_dir={"":"src"},
