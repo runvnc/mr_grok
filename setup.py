24ec9a5d213114bb44ef80013ce1ba8be15deaf0
Jason Livesay <runvnc@gmail.com>
2025-02-21 08:19:56 -0600

    init

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="mr_groq",
+    version="1.0.0",
+    packages=find_packages(where="src"),
+    package_dir={"":"src"},
+    install_requires=[
+        "openai"
+    ],
+)
